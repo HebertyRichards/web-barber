8d2905c494dd4e0daff2426ae363b1ed4287e9b0
HebertyRichards <heberynho666@hotmail.com>
2025-03-16 23:00:28 -0300

    adição de componentes da página assinaturas

--- src/pages/assinaturas/Signature.jsx
+++ src/pages/assinaturas/Signature.jsx
@@ -20,6 +20,116 @@ function Assinatura() {
           premium.
         </p>
       </div>
+      <div className="plans">
+        <div className="bronze">
+          <div className="bronze-pay">
+            <h3>COMBO BRONZE</h3>
+            <h2>R$70/ por mês</h2>
+          </div>
+          <div className="bronze-info">
+            <table>
+              <tr>
+                <td>3 Cortes por mês</td>
+              </tr>
+              <tr>
+                <td>Acabamento ilimitado</td>
+              </tr>
+              <tr>
+                <td>Nenhum</td>
+              </tr>
+              <tr>
+                <td>Nenhum</td>
+              </tr>
+              <tr>
+                <td>Nenhum</td>
+              </tr>
+              <tr>
+                <td>Nehnum</td>
+              </tr>
+              <tr>
+                <td>20% Desc. produto/serviço</td>
+              </tr>
+              <tr>
+                <td>
+                  <button>ASSINAR</button>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="silver">
+          <div className="silver-pay">
+            <h3>COMBO PRATA</h3>
+            <h2>R$110/ por mês</h2>
+          </div>
+          <div className="silver-info">
+            <table>
+              <tr>
+                <td>5 Cortes por mês</td>
+              </tr>
+              <tr>
+                <td>Acabamento ilimitado</td>
+              </tr>
+              <tr>
+                <td>Sobrancelha ilimitado</td>
+              </tr>
+              <tr>
+                <td>Hidratação</td>
+              </tr>
+              <tr>
+                <td>Nenhum</td>
+              </tr>
+              <tr>
+                <td>Nehnum</td>
+              </tr>
+              <tr>
+                <td>20% Desc. produto/serviço</td>
+              </tr>
+              <tr>
+                <td>
+                  <button>ASSINAR</button>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="gold">
+          <div className="gold-pay">
+            <h3>COMBO OURO</h3>
+            <h2>R$210/ por mês</h2>
+          </div>
+          <div className="gold-info">
+            <table>
+              <tr>
+                <td>Corte ilimitado</td>
+              </tr>
+              <tr>
+                <td>Sobrancelha ilimitado</td>
+              </tr>
+              <tr>
+                <td>Barba ilimitada</td>
+              </tr>
+              <tr>
+                <td>Depilação orelha ou nariz</td>
+              </tr>
+              <tr>
+                <td>Nutrição</td>
+              </tr>
+              <tr>
+                <td>Hidratação</td>
+              </tr>
+              <tr>
+                <td>30% Desc. produto/serviço</td>
+              </tr>
+              <tr>
+                <td>
+                  <button>ASSINAR</button>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
