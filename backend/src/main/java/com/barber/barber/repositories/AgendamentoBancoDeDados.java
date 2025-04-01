e13c1f5f54bebe656ec9991046477ce9164cabf6
HebertyRichards <heberynho666@hotmail.com>
2025-04-01 19:05:43 -0300

    redeploy back end em express

--- backend/src/main/java/com/barber/barber/repositories/AgendamentoBancoDeDados.java
+++
@@ -1,9 +0,0 @@
-package com.barber.barber.repositories;
-
-import com.barber.barber.Agendamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AgendamentoBancoDeDados extends JpaRepository<Agendamento, Long> {
-}
