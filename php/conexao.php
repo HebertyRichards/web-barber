325eff080f8d44f9b1a39d1ad87d81db7ec407b1
HebertyRichards <heberynho666@hotmail.com>
2025-04-25 11:11:08 -0300

    exclusao do php

--- php/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config = include(__DIR__ . '../php/.env.php'); 
-
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
-  }
-?>
