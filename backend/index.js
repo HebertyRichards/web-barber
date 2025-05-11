b06b3ebd0a6c567fbbcdad9a37ba1ffbcde2cd9c
HebertyRichards <heberynho666@hotmail.com>
2025-05-10 22:00:07 -0300

    commit

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,16 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
    "https://web-barber-phi.vercel.app"
   );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); 
+  }
+
   next();
 });
+
 
 /* configuração para conexão do banco de dados, 
 máximo de conexões simultaneas: 10 
--- src/components/Sobre.jsx
+++ src/components/Sobre.jsx
@@ -4,7 +4,7 @@ function Sobre() {
   return (
     <>
       <div className="sobre">
-        <h2>Sobre Nós</h2>
+        <h2>SOBRE NÓS</h2>
         <p>
           A Barbearia Ramos é uma empresa que une tradição e inovação para
           facilitar o dia a dia dos nossos clientes. Oferecemos um sistema de
